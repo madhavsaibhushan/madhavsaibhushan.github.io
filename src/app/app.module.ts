f82ab5cbf651e187968977b615cb465139c7c741
madhav <madhavsaibhushan@gmail.com>
2021-12-16 16:09:00 +0530

    intial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,10 +18,6 @@ import { AppComponent } from './app.component';
 // Import containers
 import { DefaultLayoutComponent } from './containers';
 
-import { P404Component } from './views/error/404.component';
-import { P500Component } from './views/error/500.component';
-import { LoginComponent } from './views/login/login.component';
-import { RegisterComponent } from './views/register/register.component';
 
 const APP_CONTAINERS = [
   DefaultLayoutComponent
@@ -64,10 +60,7 @@ import { HomeComponent } from './home/home.component';
   declarations: [
     AppComponent,
     ...APP_CONTAINERS,
-    P404Component,
-    P500Component,
-    LoginComponent,
-    RegisterComponent,
+    
     HomeComponent
   ],
   providers: [
